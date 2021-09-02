1aea5b350fbf12bcc36dbfab6d6c5039aaa961c2
DEA_ASILVA <lnrxnxxavgey5zfz5tff5q2237wyxsvzv4dq63aa2mg7mymflgcq>
2021-09-02 15:12:26 -0300

    Inicial

---
+++ src/main/java/terebinto/gerar/script/GerarScript.java
@@ -0,0 +1,67 @@
+package terebinto.gerar.script;
+
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+
+
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+
+public class GerarScript {
+
+    private static final String FILE_NAME = "/tmp/Camila.xlsx";
+
+    public static void main(String[] args) {
+
+        XSSFWorkbook workbook = new XSSFWorkbook();
+        XSSFSheet sheet = workbook.createSheet("Datatypes in Java");
+        Object[][] datatypes = {
+                {"Datatype", "Type", "Size(in bytes)"},
+                {"int", "Primitive", 2111},
+                {"float", "Primitive", 4},
+                {"double", "Primitive", 8},
+                {"char", "Primitive", 1},
+                {"String", "Non-Primitive", "No fixed size"}
+        };
+
+        int rowNum = 0;
+        System.out.println("Creating excel");
+
+        for (Object[] datatype : datatypes) {
+            Row row = sheet.createRow(rowNum++);
+            int colNum = 0;
+            for (Object field : datatype) {
+                Cell cell = row.createCell(colNum++);
+                if (field instanceof String) {
+                    cell.setCellValue((String) field);
+                } else if (field instanceof Integer) {
+                    cell.setCellValue((Integer) field);
+                }
+            }
+        }
+
+        try {
+            FileOutputStream outputStream = new FileOutputStream(FILE_NAME);
+            workbook.write(outputStream);
+            //workbook.cl
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Done");
+    }
+
+
+
+
+
+
+}
---
+++ src/main/java/terebinto/gerar/script/ReadExcelDemo.java
@@ -0,0 +1,94 @@
+package terebinto.gerar.script;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.*;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+
+public class ReadExcelDemo {
+    public static void main(String[] args) throws IOException {
+        ArrayList<String> id = new ArrayList<>();
+        ArrayList<String> obiud = new ArrayList<>();
+        int linhas = 0;
+
+        String diretorio = "/tmp/";
+        String nomeArquivo = "teste1";
+
+        try {
+            FileInputStream file = new FileInputStream(new File(diretorio + nomeArquivo + ".xlsx"));
+            //Create Workbook instance holding reference to .xlsx file
+            XSSFWorkbook workbook = new XSSFWorkbook(file);
+            //Get first/desired sheet from the workbook
+            XSSFSheet sheet = workbook.getSheetAt(0);
+            Row header = sheet.getRow(0);
+            int n = header.getLastCellNum();
+            linhas = n + 1;
+            //Iterate through each rows one by one
+            Iterator<Row> rowIterator = sheet.iterator();
+
+            Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+            String date = new SimpleDateFormat("ddMMyyyyss").format(timestamp.getTime());
+            FileWriter arq = new FileWriter(diretorio + nomeArquivo.concat(date) + ".sql");
+            PrintWriter gravarArq = new PrintWriter(arq);
+
+            while (rowIterator.hasNext()) {
+                Row row = rowIterator.next();
+                //For each row, iterate through all the columns
+                Iterator<Cell> cellIterator = row.cellIterator();
+                Number obuild = new Long(0);
+                String sid = "";
+
+                if (row.getRowNum() > 0) {
+
+                    int cont = 0;
+
+
+                    while (cellIterator.hasNext()) {
+                        Cell cell = cellIterator.next();
+
+
+                        switch (cell.getCellType()) {
+                            case Cell.CELL_TYPE_NUMERIC:
+
+                                if (cont == 1) {
+                                    obuild = cell.getNumericCellValue();
+
+                                }
+                                cont++;
+                                break;
+                            case Cell.CELL_TYPE_STRING:
+
+                                if (cont == 0) {
+                                    sid = cell.getStringCellValue();
+                                }
+
+                                cont++;
+                                break;
+                        }
+
+                    }
+
+                    if (obuild.longValue() > 0) {
+                        String script = "update compras.identificador set obuid ='" + obuild.longValue() + "', id_cliente= 3270840 Where id = " + sid + " and obuid is null;" + "\n";
+                        gravarArq.printf(script);
+                    }
+                }
+
+
+            }
+            file.close();
+            arq.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
+
